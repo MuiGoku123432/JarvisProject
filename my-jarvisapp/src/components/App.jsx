72da88a9b517f74ae814b5b50408ad57f799cc10
MuiGoku123432 <connortheshark705@gmail.com>
2024-05-18 00:42:05 -0500

    just made the button do stuff to work on understanding of front and back end communication

--- my-jarvisapp/src/components/App.jsx
+++ my-jarvisapp/src/components/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const App = () => {
   const [message, setMessage] = useState('');
+  const [bgColor, setBgColor] = useState('')
 
   useEffect(() => {
     axios.get('http://localhost:5000/say-hello')
@@ -14,10 +15,21 @@ const App = () => {
       });
   }, []);
 
+  const handleClick = () => {
+    axios.post('http://localhost:5000/button-clicked')
+      .then(response => {
+        setMessage(response.data.message);
+        setBgColor(response.data.color)
+      })
+      .catch(error => {
+        console.error('There was an error making the request!', error);
+      });
+  }
+
   return (
-    <div className="container mx-auto p-4">
+     <div className="container mx-auto p-4" style={{ backgroundColor: bgColor }}>
       <h1 className="text-3xl font-bold underline">{message}</h1>
-      <button className="btn btn-primary mt-4">DaisyUI Button</button>
+      <button className="btn btn-primary mt-4" onClick={handleClick}>Change Background to White</button>
     </div>
   );
 };
--- my-jarvisapp/src/python/app.py
+++ my-jarvisapp/src/python/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify # type: ignore
 
 app = Flask(__name__)
 
 
 @app.route("/say-hello", methods=["GET"])
 def say_hello():
-    return jsonify(message="Hello from Python J.A.R.V.I.S! 5")
+    return jsonify(message="Hello from Python J.A.R.V.I.S! 6")
+
+@app.route("/button-clicked", methods=["POST"])
+def buttonClicked():
+    print("Button clicked from python backend")
+    return jsonify(message="Button clicked, python to front end communication successful!", color='white')
 
 
 if __name__ == "__main__":
