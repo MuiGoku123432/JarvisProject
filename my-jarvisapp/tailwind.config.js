c7d805ac74dbef5cb8bbb57fdab766b29d9f4b39
MuiGoku123432 <connortheshark705@gmail.com>
2024-05-21 22:25:08 -0500

    Added es lint i think

--- my-jarvisapp/tailwind.config.js
+++ my-jarvisapp/tailwind.config.js
@@ -3,9 +3,32 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+ daisyui: {
+      themes: [
+        {
+          mytheme: {
+          
+          "primary": "#0284c7",
+          
+          "secondary": "#38bdf8",
+          
+          "accent": "#67e8f9",
+          
+          "neutral": "#111827",
+          
+          "base-100": "#1f2937",
+          
+          "info": "#009ade",
+
+          "success": "#7ff878",
+          
+          "warning": "#f59e0b",
+          
+          "error": "#f43f5e",
+          },
+        },
+      ],
+    },
   plugins: [
     require('daisyui'),
   ],
